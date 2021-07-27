f298554ed9e6feb3403c4d05febc68ad46a839a9
Runxuan Shu <runxuan.shu@unity3d.com>
2021-07-27 18:05:12 +0800

    Added a simple CLI interface for Y3Hack

---
+++ AssetStudioCLI/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace AssetStudioCLI
+{
+    class Program
+    {
+        private static List<string> inputFiles = null;
+        static void Main(string[] args)
+        {
+            var result = Parser.Default.ParseArguments<Properties.Settings>(args)
+                .WithParsed(options => Properties.Settings.Default = options);
+
+            if (!File.Exists(Properties.Settings.Default.input) &&
+                !Directory.Exists(Properties.Settings.Default.input))
+            {
+                Console.WriteLine("Input File/Folder Doesn't Exist!");
+                return;
+            }
+
+            FileAttributes attr = File.GetAttributes(Properties.Settings.Default.input);
+            inputFiles = new List<string>();
+            if (!attr.HasFlag(FileAttributes.Directory))
+            {
+                inputFiles.Add(Properties.Settings.Default.input);
+            }
+            else
+            {
+                ProcessDirectory(Properties.Settings.Default.input);
+            }
+
+            Studio.assetsManager.LoadFiles(inputFiles.ToArray());
+            var (productName, treeNodeCollection) = Studio.BuildAssetData();
+            //Console.WriteLine(Studio.exportableAssets.Count);
+
+            if (!Directory.Exists(Properties.Settings.Default.output))
+            {
+                Directory.CreateDirectory(Properties.Settings.Default.output);
+            }
+
+            List<AssetItem> exportAssets = Studio.exportableAssets.Where(x => x.Type != AssetStudio.ClassIDType.Sprite).ToList();
+
+            Studio.ExportAssets(Properties.Settings.Default.output, exportAssets, ExportType.Convert);
+        }
+
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                inputFiles.Add(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+    }
+}
---
+++ AssetStudioCLI/Properties.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace AssetStudioCLI.Properties
+{
+    class Settings
+    {
+        public bool displayAll = false;
+        public bool enablePreview = true;
+        public bool displayInfo = true;
+        public bool openAfterExport = false;
+        public int assetGroupOption = 1;
+        public bool convertTexture = true;
+        public bool convertAudio = true;
+        public AssetStudio.ImageFormat convertType = AssetStudio.ImageFormat.Png;
+        public bool eulerFilter = true;
+        public float filterPrecision = 0.25f;
+        public bool exportAllNodes = true;
+        public bool exportSkins = true;
+        public bool exportAnimations = true;
+        public float boneSize = 10.0f;
+        public int fbxVersion = 3;
+        public int fbxFormat = 0;
+        public float scaleFactor = 1.0f;
+        public bool exportBlendShape = true;
+        public bool castToBone = false;
+        public bool restoreExtensionName = true;
+        public bool exportAllUvsAsDiffuseMaps = false;
+
+        [Option('i', "input", Required = true, HelpText = "Input File/Folder")]
+        public string input { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output Folder")]
+        public string output { get; set; }
+
+        public static Settings Default = new Settings();
+    }
+}
